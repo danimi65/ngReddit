61c5c97997d17330b27c597db6e740681236fd85
Danika Harada <danimi65@gmail.com>
2017-03-15 22:02:16 -1000

    set up folders

--- src/app/app.js
+++ src/app/app.js
@@ -1,25 +1,40 @@
+'use strict';
+
 import angular from 'angular';
+import { DefaultState, DefaultCtrl } from './default';
+import * as uiRouter from 'angular-ui-router';
+
+
+console.log(DefaultState);
+
 
 import '../style/app.css';
 
 let app = () => {
   return {
     template: require('./app.html'),
-    controller: 'AppCtrl',
-    controllerAs: 'app'
+    // controller: 'AppCtrl',
+    // controllerAs: 'app'
   }
 };
 
-class AppCtrl {
-  constructor() {
-    this.url = 'https://github.com/preboot/angular-webpack';
-  }
-}
+// class AppCtrl {
+//   constructor() {
+//     this.url = 'https://github.com/preboot/angular-webpack';
+//   }
+// }
 
 const MODULE_NAME = 'app';
 
-angular.module(MODULE_NAME, [])
+angular.module(MODULE_NAME, ['ui.router'])
+  .config(($stateProvider) =>{
+    $stateProvider
+      .state(DefaultState.name, DefaultState);
+  })
+  .run(($state) =>{
+    $state.go('default');
+  })
   .directive('app', app)
-  .controller('AppCtrl', AppCtrl);
+  .controller(DefaultState.controller, DefaultCtrl);
 
 export default MODULE_NAME;
